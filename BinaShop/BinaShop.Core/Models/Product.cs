c521f71a648e3ef93c9a5ed1496b9a794b5aeb4c
Yaniv <yanivabu8@gmail.com>
2018-12-13 15:00:44 +0200

    Product Model and InMemory Product Repository built

---
+++ BinaShop/BinaShop.Core/Models/Product.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinaShop.Core.Models
+{
+    public class Product
+    {
+        public string Id { get; set; }
+        
+        [StringLength(30)]
+        [DisplayName("שם המוצר")]
+        public string Name { get; set; }
+        public string Description { get; set; }
+
+        [Range(0, 1500)]
+        [DisplayName("מחיר")]
+        public decimal Price { get; set; }
+        public string Category { get; set; }
+        public string Image { get; set; }
+
+        public Product()
+        {
+            this.Id = Guid.NewGuid().ToString();
+        }
+    }
+}
---
+++ BinaShop/BinaShop.DataAccess.InMemory/ProductRepository.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Caching;
+using BinaShop.Core.Models;
+
+namespace BinaShop.DataAccess.InMemory
+{
+    public class ProductRepository
+    {
+        ObjectCache cache = MemoryCache.Default;
+        List<Product> products = new List<Product>();
+
+        public ProductRepository()
+        {
+            products = cache["products"] as List<Product>;
+            if (products == null)
+            {
+                products = new List<Product>();
+            }
+        }
+
+        public void Commit()
+        {
+            cache["products"] = products;
+        }
+
+        public void Insert(Product p)
+        {
+            products.Add(p);
+        }
+
+        public void Update(Product product)
+        {
+            Product productToUpdate = products.Find(p => p.Id == product.Id);
+
+            if (productToUpdate != null)
+            {
+                productToUpdate = product;
+            }
+            else
+            {
+                throw new Exception("מוצר לא נמצא");
+            }
+        }
+        public Product Find(string Id)
+        {
+            Product product = products.Find(p => p.Id == Id);
+
+            if (product != null)
+            {
+                return product;
+            }
+            else
+            {
+                throw new Exception("מוצר לא נמצא");
+            }
+        }
+        public IQueryable<Product> Collectiom()
+        {
+            return products.AsQueryable();
+        }
+
+        public void Delete(string Id)
+        {
+            Product productToDelete = products.Find(p => p.Id == Id);
+            if (productToDelete != null)
+            {
+                products.Remove(productToDelete);
+            }
+            else
+            {
+                throw new Exception("מוצר לא נמצא");
+            }
+        }
+    }
+}
