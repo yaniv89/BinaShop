53ec5985db1da486348a3811a7071454f822c95e
Yaniv <yanivabu8@gmail.com>
2018-12-13 13:54:53 +0200

    Initial Solution Setup

---
+++ BinaShop/BinaShop.WebUI/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(BinaShop.WebUI.Startup))]
+namespace BinaShop.WebUI
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
