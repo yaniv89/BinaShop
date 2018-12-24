f48201be9461c626c9a1a077687b5663b86f3759
Yaniv <yanivabu8@gmail.com>
2018-12-24 19:44:38 +0200

    Implemented SQL Repository

---
+++ BinaShop/BinaShop.DataAccess.SQL/SQLRepository.cs
@@ -0,0 +1,58 @@
+﻿using BinaShop.Core.Contracts;
+using BinaShop.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinaShop.DataAccess.SQL
+{
+    public class SQLRepository<D> : IRepository<D> where D : BaseEntity
+    {
+        internal DataContext context;
+        internal DbSet<D> dbSet;
+
+        public SQLRepository(DataContext context)
+        {
+            this.context = context;
+            this.dbSet = context.Set<D>();
+        }
+        public IQueryable<D> Collection()
+        {
+            return dbSet;
+        }
+
+        public void Commit()
+        {
+            context.SaveChanges();
+        }
+
+        public void Delete(string Id)
+        {
+            var d = Find(Id);
+            if (context.Entry(d).State == EntityState.Detached)
+            {
+                dbSet.Attach(d);
+            }
+            dbSet.Remove(d);
+        }
+
+        public D Find(string Id)
+        {
+            return dbSet.Find(Id);
+        }
+
+        public void Insert(D d)
+        {
+            dbSet.Add(d);
+        }
+
+        public void Update(D d)
+        {
+            dbSet.Attach(d);
+            context.Entry(d).State = EntityState.Modified;
+        }
+    }
+}
--- BinaShop/BinaShop.WebUI/App_Start/UnityConfig.cs
+++ BinaShop/BinaShop.WebUI/App_Start/UnityConfig.cs
@@ -1,6 +1,7 @@
 using BinaShop.Core.Contracts;
 using BinaShop.Core.Models;
 using BinaShop.DataAccess.InMemory;
+using BinaShop.DataAccess.SQL;
 using System;
 
 using Unity;
@@ -45,8 +46,8 @@ namespace BinaShop.WebUI
 
             // TODO: Register your type's mappings here.
             // container.RegisterType<IProductRepository, ProductRepository>();
-            container.RegisterType<IRepository<Product>, InMemoryRepository<Product>>();
-            container.RegisterType<IRepository<ProductCategory>, InMemoryRepository<ProductCategory>>();
+            container.RegisterType<IRepository<Product>, SQLRepository<Product>>();
+            container.RegisterType<IRepository<ProductCategory>, SQLRepository<ProductCategory>>();
         }
     }
 }
