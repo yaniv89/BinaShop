d3c60f19b92491ae947611fb367ed9fa2c5c1efd
Yaniv <yanivabu8@gmail.com>
2019-01-01 12:23:22 +0200

    Product filters created

---
+++ BinaShop/BinaShop.Core/ViewModels/ProductListViewModel.cs
@@ -0,0 +1,15 @@
+﻿using BinaShop.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinaShop.Core.ViewModels
+{
+    public class ProductListViewModel
+    {
+        public IEnumerable<Product> Products { get; set; }
+        public IEnumerable<ProductCategory> ProductCategories { get; set; }
+    }
+}
--- BinaShop/BinaShop.WebUI/Controllers/HomeController.cs
+++ BinaShop/BinaShop.WebUI/Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 ﻿using BinaShop.Core.Contracts;
 using BinaShop.Core.Models;
+using BinaShop.Core.ViewModels;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -37,10 +38,25 @@ namespace BinaShop.WebUI.Controllers
             return View();
         }
 
-        public ActionResult Products()
+        public ActionResult Products(string Category = null)
         {
             List<Product> products = context.Collection().ToList();
-            return View(products);
+            List<ProductCategory> categories = productCategories.Collection().ToList();
+
+            if (Category == null)
+            {
+                products = context.Collection().ToList();
+            }
+            else
+            {
+                products = context.Collection().Where(p => p.Category == Category).ToList();
+            }
+
+            ProductListViewModel model = new ProductListViewModel();
+            model.Products = products;
+            model.ProductCategories = categories;
+
+            return View(model);
         }
 
         public ActionResult Details(string Id)
