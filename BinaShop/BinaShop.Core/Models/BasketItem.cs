e79d13997e28428ab9bf48f21e0865dfcea20cde
Yaniv <yanivabu8@gmail.com>
2019-01-01 13:43:08 +0200

    Added Basket Model

---
+++ BinaShop/BinaShop.Core/Models/BasketItem.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinaShop.Core.Models
+{
+    public class BasketItem : BaseEntity
+    {
+        public string BasketId { get; set; }
+        public string ProuductId { get; set; }
+        public int Quantity { get; set; }
+    }
+}
--- BinaShop/BinaShop.DataAccess.SQL/DataContext.cs
+++ BinaShop/BinaShop.DataAccess.SQL/DataContext.cs
@@ -18,5 +18,7 @@ namespace BinaShop.DataAccess.SQL
 
         public DbSet<Product> Products { get; set; }
         public DbSet<ProductCategory> ProductCategories { get; set; }
+        public DbSet<Basket> Baskets { get; set; }
+        public DbSet<BasketItem> BasketItems { get; set; }
     }
 }
