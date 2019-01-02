bad931e26cfe6f4c4b743159e9c86f07f30e0f88
Yaniv <yanivabu8@gmail.com>
2019-01-02 19:19:57 +0200

    Added Tests

---
+++ BinaShop/BinaShop.WebUI.Tests/Controllers/HomeControllerTests.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BinaShop.WebUI;
+using BinaShop.WebUI.Controllers;
+using BinaShop.Core.Contracts;
+using BinaShop.Core.Models;
+using BinaShop.Core.ViewModels;
+
+namespace BinaShop.WebUI.Tests.Controllers
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void ProductPageDoesReturnProducts()
+        {
+            IRepository<Product> productContext = new Mocks.MockContext<Product>();
+            IRepository<ProductCategory> productCategoryContext = new Mocks.MockContext<ProductCategory>();
+
+            productContext.Insert(new Product());
+
+            HomeController controller = new HomeController(productContext, productCategoryContext);
+
+            var result = controller.Products() as ViewResult;
+            var viewModel = (ProductListViewModel) result.ViewData.Model;
+
+            Assert.AreEqual(1, viewModel.Products.Count());
+        }
+    }
+}
---
+++ BinaShop/BinaShop.WebUI.Tests/Mocks/MockContext.cs
@@ -0,0 +1,75 @@
+﻿using BinaShop.Core.Contracts;
+using BinaShop.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinaShop.WebUI.Tests.Mocks
+{
+    public class MockContext<D>: IRepository<D> where D: BaseEntity
+    {
+
+        List<D> items;
+        string className;
+        public MockContext()
+        {
+           items=new List<D>();
+
+        }
+        public void Commit()
+        {
+            return;
+        }
+
+        public void Insert(D d)
+        {
+            items.Add(d);
+        }
+
+        public void Update(D d)
+        {
+            D dToUpdate = items.Find(i => i.Id == d.Id);
+            if (dToUpdate != null)
+            {
+                dToUpdate = d;
+            }
+            else
+            {
+                throw new Exception(className + "לא נמצא");
+            }
+        }
+
+        public D Find(string Id)
+        {
+            D d = items.Find(i => i.Id == Id);
+            if (d != null)
+            {
+                return d;
+            }
+            else
+            {
+                throw new Exception(className + "לא נמצא");
+            }
+        }
+
+        public IQueryable<D> Collection()
+        {
+            return items.AsQueryable();
+        }
+
+        public void Delete(string Id)
+        {
+            D dToDelete = items.Find(i => i.Id == Id);
+            if (dToDelete != null)
+            {
+                items.Remove(dToDelete);
+            }
+            else
+            {
+                throw new Exception(className + "לא נמצא");
+            }
+        }
+    }
+}
