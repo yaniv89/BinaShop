c02bc5fb22420e94d1584ce2fc5241845bde4bfc
Yaniv <yanivabu8@gmail.com>
2019-01-03 20:16:31 +0200

    Added Order Manager

---
+++ BinaShop/BinaShop.WebUI/Controllers/OrderManagerController.cs
@@ -0,0 +1,48 @@
+﻿using BinaShop.Core.Contracts;
+using BinaShop.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BinaShop.WebUI.Controllers
+{
+    public class OrderManagerController : Controller
+    {
+        IOrderService orderService;
+
+        public OrderManagerController(IOrderService OrderService)
+        {
+            this.orderService = OrderService;
+        }
+        // GET: OrderManager
+        public ActionResult Index()
+        {
+            List<Order> orders = orderService.GetOrderList();
+            return View(orders);
+        }
+
+        public ActionResult UpdateOrder(string Id)
+        {
+            ViewBag.StatusList = new List<string>()
+            {
+                "הזמנה נוצרה",
+                "תשלום בוצע",
+                "הזמנה נשלחה",
+                "הזמנה הושלמה"
+            };
+            Order order = orderService.GetOrder(Id);
+            return View(order);
+        }
+        [HttpPost]
+        public ActionResult UpdateOrder(Order updatedorder,string Id)
+        {
+            Order order = orderService.GetOrder(Id);
+
+            order.OrderStatus = updatedorder.OrderStatus;
+            orderService.UpdateOrder(order);
+            return RedirectToAction("Index");
+        }
+    }
+}
